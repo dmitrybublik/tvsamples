eac9cf6a644e060b0fd725344e7ab0716d9dc35a
Dmitry Bublik <dmitry.buba@gmail.com>
2023-04-19 11:38:06 +0400

    fix "build" script

---
+++ out/_next/static/8e4QbhaWVXlV9PHtUFc7g/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/955229c28454e75a.css","static/chunks/pages/index-25611f7dc363d3a8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
