f817133f8bad42d97a28b8d6f111cd598faad097
Dmitry Bublik <dmitry.buba@gmail.com>
2023-04-19 11:36:11 +0400

    decrease nextjs version to v11

--- out/_next/static/hVuT-aB7CsmjyCV9zk0aL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8e23e2be85300958.css","static/chunks/pages/index-5cc2c6ea5680abee.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
